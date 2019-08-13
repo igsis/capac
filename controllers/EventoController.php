33facbd556514bf6b781c1e5efe7e185ece78aa1
Lorelei Gabriele <loreleigab@hotmail.com>
2019-08-13 12:24:29 -0300

    Controlador de evento

---
+++ controllers/EventoController.php
@@ -0,0 +1,34 @@
+<?php
+
+include_once '../models/ConnectionModel.php';
+include_once '../models/ManagerModel.php';
+
+$manager = new ManagerModel();
+
+$data = $_POST;
+
+if(isset($_POST['cadastra'])){
+    if(isset($data) && !empty($data)) {
+        $manager->insert("eventos", $data);
+
+        header("Location: ../index.php?evento_add");
+    }
+}
+
+if(isset($_POST['edita'])){
+    $id = $_POST['id'];
+    if(isset($id) && !empty($id)) {
+        $manager->update("eventos", $data, $id);
+
+        header("Location: ../index.php?evento_update");
+    }
+}
+
+if(isset($_POST['apaga'])){
+    $id = $_POST['id'];
+    if(isset($id) && !empty($id)) {
+        $manager->update("eventos", $data, $id);
+
+        header("Location: ../index.php?evento_delete");
+    }
+}
