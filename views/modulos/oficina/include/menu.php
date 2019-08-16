70a2d2ad499b4b32030f7517c5dff17c22b71446
Lorelei <loreleigab@hotmail.com>
2019-08-16 14:16:04 -0300

    Menu de oficina provisório

--- views/modulos/oficina/include/menu.php
+++ views/modulos/oficina/include/menu.php
@@ -1,32 +1,48 @@
-<li class="nav-item has-treeview menu-open">
-    <a href="#" class="nav-link active">
-        <i class="nav-icon fas fa-tachometer-alt"></i>
-        <p>
-            Menu Dinamico
-            <i class="right fas fa-angle-left"></i>
-        </p>
+<li class="nav-item">
+    <a href="<?= SERVERURL ?>oficina/evento_cadastro" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>Evento</p>
     </a>
-    <ul class="nav nav-treeview">
-        <li class="nav-item">
-            <a href="#" class="nav-link active">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Aeooo</p>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="#" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Inactive Page</p>
-            </a>
-        </li>
-    </ul>
 </li>
 <li class="nav-item">
-    <a href="#" class="nav-link">
-        <i class="nav-icon fas fa-th"></i>
-        <p>
-            Simple Link
-            <span class="right badge badge-danger">New</span>
-        </p>
+    <a href="<?= SERVERURL ?>oficina/atracao_cadastro" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>Atração</p>
+    </a>
+</li>
+<li class="nav-item">
+    <a href="<?= SERVERURL ?>oficina/produtor_cadastro" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>Produtor</p>
+    </a>
+</li>
+<li class="nav-item">
+    <a href="<?= SERVERURL ?>oficina/complemento_oficina_cadastro" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>Oficina</p>
+    </a>
+</li>
+<li class="nav-item">
+    <a href="<?= SERVERURL ?>oficina/arquivos_com_prod" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        Arquivos <br> Comunicação/Produtor
+    </a>
+</li>
+<li class="nav-item">
+    <a href="<?= SERVERURL ?>oficina/pf_cadastro" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>PF</p>
+    </a>
+</li>
+<li class="nav-item">
+    <a href="<?= SERVERURL ?>oficina/pj_cadastro" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>PJ</p>
+    </a>
+</li>
+<li class="nav-item">
+    <a href="<?= SERVERURL ?>oficina/representante_cadastro" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+        <p>Representante Legal</p>
     </a>
 </li>
