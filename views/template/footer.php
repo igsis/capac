c7310ffc0e4e896a5ac0b46dd6b09301ce307a97
Lorelei Gabriele <loreleigab@hotmail.com>
2019-08-22 17:19:00 -0300

    Loco correto no rodapé

--- views/template/footer.php
+++ views/template/footer.php
@@ -1,6 +1,6 @@
 <!-- To the right -->
 <div class="float-right d-none d-sm-inline">
-    <img src="<?= SERVERURL?>views/dist/img/logo_cultura.jpg">
+    <img src="<?= SERVERURL?>views/dist/img/CULTURA_HORIZONTAL_pb_positivo.png">
 </div>
 <!-- Default to the left -->
 
