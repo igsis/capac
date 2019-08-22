9ea3a16f7658cc72b901c6b52442698f1db3bfb3
Diego Vicente <diego.vdossantos@gmail.com>
2019-08-22 16:45:07 -0300

    Dados de session + tempo de session

--- config/configGeral.php
+++ config/configGeral.php
@@ -2,3 +2,4 @@
 define('SERVERURL', "http://{$_SERVER['HTTP_HOST']}/capac/");
 define('NOMESIS', "CAPAC");
 date_default_timezone_set('America/Sao_Paulo');
+ini_set('session.gc_maxlifetime', 60*60); // 60 minutos
