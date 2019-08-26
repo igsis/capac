b961323bd094c0c668735d53024d2e51d7aa4052
Lorelei Gabriele <loreleigab@hotmail.com>
2019-08-26 14:08:05 -0300

    Início do início

--- views/modulos/inicio/inicio.php
+++ views/modulos/inicio/inicio.php
@@ -16,37 +16,37 @@
         <!-- Small Box (Stat card) -->
         <h5 class="mb-2 mt-4"></h5>
         <div class="row">
-            <div class="col-lg-2 col-6">
+            <div class="col-md-3">
                 <!-- small card -->
-                <div class="small-box bg-gradient-primary">
-                    <div class="inner">
-                        <h3>Evento</h3>
+                <div class="card card-primary card-outline">
+                    <div class="card-header">
+                        <h3 class="card-title">
+                            <i class="fas fa-calendar-alt"></i>
+                            Evento
+                        </h3>
                         <p>Com cachê</p>
-                    </div>
-                    <div class="icon">
-                        <i class="fas fa-calendar-alt"></i>
                     </div>
                     <div class="bg-light col-md">
                         <p>Aqui são inseridas as informações sobre o seu evento com cachê, incluindo pessoa jurídica e/ou física.</p>
                     </div>
+
                     <a href="<?= SERVERURL ?>eventos/inicio" class="small-box-footer">
                         Acesse <i class="fas fa-arrow-circle-right"></i>
                     </a>
                 </div>
             </div>
             <!-- ./col -->
-            <div class="col-sm-1">&nbsp;</div>
-            <!-- ./col -->
-            <div class="col-lg-2 col-6">
+            <div class="col-md-3">
                 <!-- small card -->
-                <div class="small-box bg-gradient-primary">
-                    <div class="inner">
-                        <h3>Evento</h3>
-                        <p>Sem Cachê</p>
+                <div class="card card-primary card-outline">
+                    <div class="card-header">
+                        <h3 class="card-title">
+                            <i class="fas fa-calendar-alt"></i>
+                            Evento
+                        </h3>
+                        <p>Sem cachê</p>
                     </div>
-                    <div class="icon">
-                        <i class="fas fa-calendar-alt"></i>
-                    </div>
+
                     <div class="bg-light col-md">
                         <p>Aqui são inseridas as informações sobre o seu evento sem cachê, incluindo pessoa jurídica e/ou física.</p>
                     </div>
@@ -56,17 +56,15 @@
                 </div>
             </div>
             <!-- ./col -->
-            <div class="col-sm-1">&nbsp;</div>
-            <!-- ./col -->
-            <div class="col-lg-2 col-6">
+            <div class="col-md-3">
                 <!-- small card -->
-                <div class="small-box bg-gradient-primary">
-                    <div class="inner">
-                        <h3>Emenda</h3>
+                <div class="card card-primary card-outline">
+                    <div class="card-header">
+                        <h3 class="card-title">
+                            <i class="fas fa-calendar-alt"></i>
+                            Emenda
+                        </h3>
                         <p>Parlamentar</p>
-                    </div>
-                    <div class="icon">
-                        <i class="fas fa-calendar-alt"></i>
                     </div>
                     <div class="bg-light col-md">
                         <p>Para evento com contratação através de emenda parlamentar, incluindo pessoa jurídica e/ou física.</p>
@@ -77,17 +75,15 @@
                 </div>
             </div>
             <!-- ./col -->
-            <div class="col-sm-1">&nbsp;</div>
-            <!-- ./col -->
-            <div class="col-lg-2 col-6">
+            <div class="col-md-3">
                 <!-- small card -->
-                <div class="small-box bg-gradient-primary">
-                    <div class="inner">
-                        <h3>Agendão</h3>
+                <div class="card card-primary card-outline">
+                    <div class="card-header">
+                        <h3 class="card-title">
+                            <i class="fas fa-calendar-alt"></i>
+                            Agendão
+                        </h3>
                         <p>Divulgação</p>
-                    </div>
-                    <div class="icon">
-                        <i class="fas fa-calendar-alt"></i>
                     </div>
                     <a href="#" class="small-box-footer">
                         Acesse <i class="fas fa-arrow-circle-right"></i>
