675039e3326760a1c5d8aa18f902202ac1d71614
Lorelei Gabriele <loreleigab@hotmail.com>
2019-08-26 11:16:28 -0300

    Representante legal

--- views/modulos/oficina/representante_cadastro.php
+++ views/modulos/oficina/representante_cadastro.php
@@ -1,15 +1,15 @@
+<?php
+$id = isset($_GET['id']) ? $_GET['id'] : null;
+require_once "./controllers/RepresentanteController.php";
+$insRepresentante = new RepresentanteController();
+$representante = $insRepresentante->recuperaRepresentante($id)->fetch();
+?>
 <!-- Content Header (Page header) -->
 <div class="content-header">
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
                 <h1 class="m-0 text-dark">Cadastro de representante legal</h1>
-            </div><!-- /.col -->
-            <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item active">Starter Page</li>
-                </ol>
             </div><!-- /.col -->
         </div><!-- /.row -->
     </div><!-- /.container-fluid -->
@@ -28,27 +28,30 @@
                     </div>
                     <!-- /.card-header -->
                     <!-- form start -->
-                    <form class="form-horizontal" method="POST" action="#" role="form">
+                    <form class="form-horizontal formulario-ajax" method="POST" action="<?= SERVERURL ?>ajax/representanteAjax.php" role="form" data-form="<?= ($id) ? "update" : "save" ?>">
+                        <input type="hidden" name="_method" value="<?= ($id) ? "editar" : "cadastrar" ?>">
+                        <?php if ($id): ?>
+                            <input type="hidden" name="id" value="<?= $id ?>">
+                        <?php endif; ?>
                         <div class="card-body">
                             <div class="row">
                                 <div class="form-group col-md-6">
                                     <label for="nome">Nome: *</label>
-                                    <input type="text" class="form-control" id="nome" name="nome" placeholder="Digite o nome completo" maxlength="120" required>
+                                    <input type="text" class="form-control" id="nome" name="nome" placeholder="Digite o nome completo" maxlength="70" value="<?= $representante['nome'] ?>" required>
                                 </div>
                                 <div class="form-group col-md-3">
                                     <label for="rg">RG: </label>
-                                    <input type="text" class="form-control" id="rg" name="rg" required maxlength="12">
+                                    <input type="text" class="form-control" id="rg" name="rg" maxlength="20" value="<?= $representante['rg'] ?>" required>
                                 </div>
                                 <div class="form-group col-md-3">
                                     <label for="cpf">CPF: </label>
-                                    <input type="text" class="form-control" id="cpf" name="cpf" required  readonly>
+                                    <input type="text" class="form-control" id="cpf" name="cpf" value="<?= $representante['cpf'] ?>" required readonly>
                                 </div>
                             </div>
                         </div>
                         <!-- /.card-body -->
                         <div class="card-footer">
                             <button type="submit" class="btn btn-info float-right">Gravar</button>
-                            <button type="submit" class="btn btn-default">Cancel</button>
                         </div>
                         <!-- /.card-footer -->
                     </form>
