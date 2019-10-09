0c5c5b1903dd83a7e0a7512efeeb48e19e909dd4
Lorelei Gabriele <loreleigab@hotmail.com>
2019-10-09 16:58:01 -0300

    Ajuste de layout na tela de login

--- views/modulos/inicio/login.php
+++ views/modulos/inicio/login.php
@@ -6,25 +6,26 @@
     }
 ?>
 <!-- Content Header (Page header) -->
+
 <div class="content-header">
     <div class="container-fluid">
-        <div class="row mb-2">
+        <div class="row mb-1">
             <div class="col-sm-10">
-                <h1 class="m-0 text-dark">CAPAC - Cadastro de Artistas e Profissionais de Arte e Cultura</h1>
+                <h1 class="m-0 text-dark"><b>CAPAC - Cadastro de Artistas e Profissionais de Arte e Cultura</b></h1>
             </div>
-            <div class="col-sm-2">
+            <div class="col-sm-2" align="right">
                 <img src="<?= SERVERURL ?>views/dist/img/CULTURA_HORIZONTAL_pb_positivo.png" alt="logo cultura">
             </div>
         </div><!-- /.row -->
     </div><!-- /.container-fluid -->
 </div>
 <!-- /.content-header -->
-
+    <div class="login-page">
 <!-- Main content -->
 <div class="content">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-lg-12">
+            <div class="offset-1 col-lg-10">
                 <div class="card">
                     <div class="card-body">
                         <div class="row">
@@ -32,12 +33,12 @@
                                 <p class="card-text"><span style="text-align: justify; display:block;">
                                     Este sistema tem por objetivo criar um ambiente para credenciamento de artistas e profissionais de arte e cultura a fim de agilizar os processos de contratação artística em eventos realizados pela Secretaria Municipal de Cultura de São Paulo.</span></p>
                                 <p class="card-text"><span style="text-align: justify; display:block;">
-                                    Uma vez cadastrados, esses artistas poderão atualizar suas informações e enviar a documentação necessária para o processo de contratação. Como o sistema possui ligação direta com o sistema da programação, a medida que o cadastro do artista no IGSIS - CAPAC encontra-se atualizado, o processo de contratação consequentemente é agilizado.</span></p>
+                                    Uma vez cadastrados, esses artistas poderão atualizar suas informações e enviar a documentação necessária para o processo de contratação. Como o sistema possui ligação direta com o sistema da programação, a medida que o cadastro do artista no CAPAC encontra-se atualizado, o processo de contratação consequentemente é agilizado.</span></p>
                                 <p class="card-text">Podem se cadastrar artistas ou grupos artísticos, como pessoa física ou jurídica.</p>
                                 <p class="card-text">Dúvidas entre em contato com o setor responsável por sua contratação.</p>
                             </div>
                             <div class="col-md-1"></div>
-                            <div class="col-md-5">
+                            <div class="col-md-5 mb-2">
                                 <div class="card"><?php if(isset($mensagem)) echo $mensagem ?>
                                     <div class="card-body login-card-body">
                                         <form action="" method="POST">
@@ -85,4 +86,5 @@
             </div>
         </div>
     </div>
+</div>
 </div>
