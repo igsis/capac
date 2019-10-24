438f4f6fa10df3737db0629946c928b4cb9abc88
Diego Vicente <diego.vdossantos@gmail.com>
2019-10-24 17:59:26 -0300

    arquivos do proponente por template

--- views/template/anexos_proponente_template.php
+++ views/template/anexos_proponente_template.php
@@ -1,31 +1,32 @@
 <?php
 require_once "./controllers/ArquivoController.php";
 require_once "./controllers/PedidoController.php";
-$arquivosObj = new ArquivoController();
 
 $pedido_id = $_SESSION['pedido_id_c'];
-$tipo_documento_id = 3;
 
 $pedidoObj = new PedidoController();
+$arquivosObj = new ArquivoController();
+
 $proponente = $pedidoObj->recuperaProponente($pedido_id);
+
 if ($proponente->pessoa_tipo_id == 1) {
-    $url_pf = SERVERURL."pdf/declaracao_exclusividade_pf.php?id=".$_SESSION['origem_id_c'];
-    $botoes="<div class=\"offset-3 col-md-6\"><a href=\"$url_pf\" class=\"btn btn-primary btn-block\" target=\"_blank\">Modelo Único - Grupo</a></div>";
+    $tipo_documento_id = 1;
+    $proponente_id = $arquivosObj->encryption($proponente->pessoa_fisica_id);
+    $url = SERVERURL."pdf/facc_pf.php?id=".$proponente_id;
 } else {
-    $url_pj_g = SERVERURL."pdf/declaracao_exclusividade_grupo_pj.php?id=".$_SESSION['origem_id_c'];
-    $url_pj_s = SERVERURL."pdf/declaracao_exclusividade_1pessoa_pj.php?id=".$_SESSION['origem_id_c'];
-    $botoes="<div class=\"offset-1 col-md-5\"><a href=\"$url_pj_g\" class=\"btn btn-primary btn-block\" target=\"_blank\">Grupo</a></div><div class=\"col-md-5\"><a href=\"$url_pj_s\" class=\"ml-md-2 btn btn-primary btn-block\" target=\"_blank\">Artista Solo</a></div>";
+    $tipo_documento_id = 2;
+    $proponente_id = $arquivosObj->encryption($proponente->pessoa_juridica_id);
+    $url = SERVERURL."pdf/facc_pj.php?id=".$proponente_id;
 }
 
 $lista_documento_ids = $arquivosObj->recuperaIdListaDocumento($tipo_documento_id)->fetchAll(PDO::FETCH_COLUMN);
 ?>
-
 <!-- Content Header (Page header) -->
 <div class="content-header">
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1 class="m-0 text-dark">Demais Anexos</h1>
+                <h1 class="m-0 text-dark">Anexos do Proponente</h1>
             </div><!-- /.col -->
         </div><!-- /.row -->
     </div><!-- /.container-fluid -->
@@ -36,7 +37,7 @@ $lista_documento_ids = $arquivosObj->recuperaIdListaDocumento($tipo_documento_id
 <div class="content">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-3">
                 <div class="card card-warning">
                     <div class="card-header">
                         <h3 class="card-title">Atenção!</h3>
@@ -55,7 +56,7 @@ $lista_documento_ids = $arquivosObj->recuperaIdListaDocumento($tipo_documento_id
                 </div>
                 <!-- /.card -->
             </div>
-            <div class="col-md-6">
+            <div class="col-md-9">
                 <div class="card card-outline card-info">
                     <div class="card-header">
                         <h3 class="card-title">Gerar documentos</h3>
@@ -70,7 +71,28 @@ $lista_documento_ids = $arquivosObj->recuperaIdListaDocumento($tipo_documento_id
                     <div class="card-body">
                         <p>Para gerar alguns dos arquivos online, utilize os links abaixo:</p>
                         <ul>
-                            <li><a href="#" onclick="alerta();">Declaração de Exclusividade</a></li>
+                            <?php
+                            if ($proponente->pessoa_tipo_id == 1) {
+                                ?>
+                                <li><a href="" target="_blank">Cartão CPF</a></li>
+                                <li><a href="" target="_blank">Declaração CCM (caso não possua)</a></li>
+                                <li><a href="http://www.tst.jus.br/certidao" target="_blank">CNDT - Certidão Negativa de Débitos de Tributos Trabalhistas</a></li>
+                                <li><a href="http://servicos.receita.fazenda.gov.br/Servicos/certidao/CNDConjuntaInter/InformaNICertidao.asp?tipo=2" target="_blank">CND Federal - Certidão de Débitos Relativos a Créditos Tributários Federais e à Dívida Ativa da União</a></li>
+                                <?php
+                            }else {
+                                ?>
+                                <li><a href="http://www.receita.fazenda.gov.br/pessoajuridica/cnpj/cnpjreva/cnpjreva_solicitacao.asp" target="_blank">Cartão CNPJ</li>
+                                <li><a href="https://cpom.prefeitura.sp.gov.br/prestador/SituacaoCadastral" target="_blank">CPOM - Cadastro de Empresas Fora do Município</a></li>
+                                <li><a href="https://www.sifge.caixa.gov.br/Cidadao/Crf/FgeCfSCriteriosPesquisa.asp" target="_blank">CRF do FGTS</a></li>
+                                <li><a href="http://www.receita.fazenda.gov.br/Aplicacoes/ATSPO/Certidao/CNDConjuntaSegVia/NICertidaoSegVia.asp?Tipo=1" target="_blank">CND Federal - Certidão de Débitos Relativos a Créditos Tributários Federais e à Dívida Ativa da União</a></li>
+                                <li><a href="http://www.receita.fazenda.gov.br/Aplicacoes/ATSPO/Certidao/certaut/CndConjunta/ConfirmaAutenticCndSolicitacao.asp?ORIGEM=PJ" target="_blank">Autenticidade de CND ­ Certidão de Débitos Relativos a Créditos Tributários Federais e à Dívida Ativa da União (CND)</a></li>
+                                <?php
+                            }
+                            ?>
+                            <li><a href="https://duc.prefeitura.sp.gov.br/certidoes/forms_anonimo/frmConsultaEmissaoCertificado.aspx" target="_blank">CTM - Certidão Negativa de Débitos Tributários Mobiliários Municipais de São Paulo</a></li>
+                            <li><a href="http://www3.prefeitura.sp.gov.br/cadin/Pesq_Deb.aspx" target="_blank">CADIN Municipal</a></li>
+                            <li><a href="https://ccm.prefeitura.sp.gov.br/login/contribuinte?tipo=F" target="_blank">FDC CCM - Ficha de Dados Cadastrais de Contribuintes Mobiliários</a></li>
+                            <li><a href="#" onclick="alerta();">FACC - Ficha de Atualização de Cadastro de Credores</a></li>
                         </ul>
                     </div>
                     <!-- /.card-body -->
@@ -89,14 +111,14 @@ $lista_documento_ids = $arquivosObj->recuperaIdListaDocumento($tipo_documento_id
                     <div class="card-body p-0">
                         <form class="formulario-ajax" method="POST" action="<?=SERVERURL?>ajax/arquivosAjax.php" data-form="save" enctype="multipart/form-data">
                             <input type="hidden" name="_method" value="enviarArquivo">
-                            <input type="hidden" name="origem_id" value="<?= $pedido_id ?>">
+                            <input type="hidden" name="origem_id" value="<?= $proponente_id ?>">
                             <input type="hidden" name="pagina" value="<?=$_GET['views']?>">
                             <table class="table table-striped">
                                 <tbody>
                                 <?php
                                 $arquivos = $arquivosObj->listarArquivos($tipo_documento_id)->fetchAll(PDO::FETCH_OBJ);
                                 foreach ($arquivos as $arquivo) {
-                                    if ($arquivosObj->consultaArquivoEnviado($arquivo->id, $pedido_id)) {
+                                    if ($arquivosObj->consultaArquivoEnviado($arquivo->id, $proponente_id)) {
                                         ?>
                                         <tr>
                                             <td colspan="2">
@@ -150,7 +172,7 @@ $lista_documento_ids = $arquivosObj->recuperaIdListaDocumento($tipo_documento_id
                             </thead>
                             <tbody>
                             <?php
-                            $arquivosEnviados = $arquivosObj->listarArquivosEnviados($pedido_id, $lista_documento_ids)->fetchAll(PDO::FETCH_OBJ);
+                            $arquivosEnviados = $arquivosObj->listarArquivosEnviados($proponente_id, $lista_documento_ids)->fetchAll(PDO::FETCH_OBJ);
                             if (count($arquivosEnviados) != 0) {
                                 foreach ($arquivosEnviados as $arquivo) {
                                     ?>
@@ -181,6 +203,7 @@ $lista_documento_ids = $arquivosObj->recuperaIdListaDocumento($tipo_documento_id
                             </tbody>
                         </table>
                     </div>
+
                 </div>
                 <!-- /.card -->
             </div>
@@ -192,24 +215,24 @@ $lista_documento_ids = $arquivosObj->recuperaIdListaDocumento($tipo_documento_id
 
 <script>
     function alerta(){
-        Swal.fire({
-            title: 'Declaração de Exclusividade',
-            html: 'A Declaração de Exclusividade é um documento necessário para sua contratação, quando se tratar de um grupo de artistas.<br>Escolha entre um dos modelos abaixo e clique no botão para gerar a Declaração de Exclusividade.<br>' +
-                '<div class="row"><?= $botoes ?></div>'+
-                '<span style="color:red">Deve ser impressa, datada e assinada nos campos indicados no documento</span>.<br>Logo após, deve-se digitaliza-la e então anexa-la ao sistema através da lista de arquivos.<br>',
-            type: 'warning',
-            allowOutsideClick: false,
-            allowEscapeKey: false,
-            showCancelButton: true,
-            showConfirmButton: false,
-            cancelButtonText: 'Fechar'
-        });
-    }
+            Swal.fire({
+                title: 'FACC - Ficha de Atualização de Cadastro de Credores',
+                html: 'A FACC é um documento necessário para recebimento do cachê. Após inserir seus dados pessoais e os dados bancários, clique no botão para gerar a FACC. <br><span style="color:red">Deve ser impressa, datada e assinada nos campos indicados no documento</span>.<br>Logo após, deve-se digitaliza-la e então anexa-la ao sistema através do campo abaixo.',
+                type: 'warning',
+                allowOutsideClick: false,
+                allowEscapeKey: false,
+                showCancelButton: false,
+                confirmButtonText: 'Confirmar'
+            }).then(function() {
+                window.open('<?=$url?>', '_blank');
+            });
+        }
 </script>
 
 <script type="application/javascript">
     $(document).ready(function () {
         $('.nav-link').removeClass('active');
-        $('#demais_anexos').addClass('active');
+        $('#itens-proponente').addClass('menu-open');
+        $('#anexos-proponente').addClass('active');
     })
 </script>
