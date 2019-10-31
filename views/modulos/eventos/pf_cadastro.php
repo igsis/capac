fecfa056ee577759b636f9f109a6f66d0f47c737
root <root@pop-os.localdomain>
2019-10-31 17:00:26 -0300

    teste

--- views/modulos/eventos/pf_cadastro.php
+++ views/modulos/eventos/pf_cadastro.php
@@ -36,30 +36,24 @@ if (isset($_POST['pf_passaporte'])){
 $cenica = $insAtracao->verificaCenica($_SESSION['origem_id_c']);
 ?>
 
-<!-- Content Header (Page header) -->
 <div class="content-header">
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
                 <h1 class="m-0 text-dark">Cadastro de pessoa física</h1>
-            </div><!-- /.col -->
-        </div><!-- /.row -->
-    </div><!-- /.container-fluid -->
+            </div>
+        </div>
+    </div>
 </div>
-<!-- /.content-header -->
 
-<!-- Main content -->
 <div class="content">
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12">
-                <!-- Horizontal Form -->
                 <div class="card card-info">
                     <div class="card-header">
                         <h3 class="card-title">Dados</h3>
                     </div>
-                    <!-- /.card-header -->
-                    <!-- form start -->
                     <form class="form-horizontal formulario-ajax" method="POST" action="<?= SERVERURL ?>ajax/pedidoFisicaAjax.php" role="form" data-form="<?= ($id) ? "update" : "save" ?>">
                         <input type="hidden" name="_method" value="<?= ($id) ? "editar" : "cadastrar" ?>">
                         <input type="hidden" name="pagina" value="eventos">
@@ -227,21 +221,14 @@ $cenica = $insAtracao->verificaCenica($_SESSION['origem_id_c']);
                             }
                             ?>
                         </div>
-                        <!-- /.card-body -->
                         <div class="card-footer">
                             <button type="submit" class="btn btn-info float-right">Gravar</button>
                         </div>
-                        <!-- /.card-footer -->
                         <div class="resposta-ajax"></div>
                     </form>
                 </div>
-                <!-- /.card -->
             </div>
         </div>
-        <!-- /.row -->
-    </div><!-- /.container-fluid -->
+    </div>
 </div>
-<!-- /.content -->
-
-
 <script src="../views/dist/js/cep_api.js"></script>
