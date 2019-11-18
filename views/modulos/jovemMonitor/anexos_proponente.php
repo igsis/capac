c8a938da5d10d016346189d9bd4215a439931044
Lucas <lucasdauto79@gmail.com>
2019-11-18 14:35:28 -0200

    Ajustes anexos Jovem Monitor

--- views/modulos/jovemMonitor/anexos_proponente.php
+++ views/modulos/jovemMonitor/anexos_proponente.php
@@ -28,7 +28,8 @@ $lista_documento_ids = $arquivosObj->recuperaIdListaDocumento($tipo_documento_id
                     <div class="card-header">
                         <h3 class="card-title">Atenção!</h3>
                         <div class="card-tools">
-                            <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse"><i
+                                        class="fas fa-minus"></i>
                             </button>
                         </div>
                     </div>
@@ -52,37 +53,44 @@ $lista_documento_ids = $arquivosObj->recuperaIdListaDocumento($tipo_documento_id
                     </div>
                     <!-- /.card-header -->
                     <!-- table start -->
-                    <div class="card-body p-0">
-                        <form class="formulario-ajax" method="POST" action="<?=SERVERURL?>ajax/arquivosAjax.php" data-form="save" enctype="multipart/form-data">
+                    <div id="lista-arquivos" class="card-body p-0">
+                        <form class="formulario-ajax" method="POST" action="<?= SERVERURL ?>ajax/arquivosAjax.php"
+                              data-form="save" enctype="multipart/form-data">
                             <input type="hidden" name="_method" value="enviarArquivo">
                             <input type="hidden" name="origem_id" value="<?= $proponente_id ?>">
                             <input type="hidden" name="pagina" value="jovemMonitor/anexos_proponente">
                             <table class="table table-striped">
                                 <tbody>
                                 <?php
+                                $cont = 0;
                                 $arquivos = $arquivosObj->listarArquivos($tipo_documento_id)->fetchAll(PDO::FETCH_OBJ);
                                 foreach ($arquivos as $arquivo) {
-                                    if ($arquivosObj->consultaArquivoEnviado($arquivo->id, $proponente_id)) {
+                                    if (!($arquivosObj->consultaArquivoEnviado($arquivo->id, $proponente_id))) {
                                         ?>
                                         <tr>
-                                            <td colspan="2">
-                                                <div class="callout callout-success text-center">
-                                                    Arquivo <strong><?= $arquivo->documento ?></strong> já enviado!
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    <?php } else { ?>
-                                        <tr>
                                             <td>
-                                                <label for=""><?=$arquivo->documento?></label>
+                                                <label for=""><?= $arquivo->documento ?></label>
                                             </td>
                                             <td>
-                                                <input type="hidden" name="<?=$arquivo->sigla?>" value="<?=$arquivo->id?>">
-                                                <input class="text-center" type='file' name='<?=$arquivo->sigla?>'><br>
+                                                <input type="hidden" name="<?= $arquivo->sigla ?>"
+                                                       value="<?= $arquivo->id ?>">
+                                                <input class="text-center" type='file'
+                                                       name='<?= $arquivo->sigla ?>'><br>
                                             </td>
                                         </tr>
                                         <?php
+                                        $cont++;
                                     }
+                                }
+
+                                if ($cont == 0){
+                                    ?>
+                                    <tr>
+                                        <td colspan="2">
+                                            Todos os arquivos já foram enviados!
+                                        </td>
+                                    </tr>
+                                <?php
                                 }
                                 ?>
                                 </tbody>
@@ -95,12 +103,13 @@ $lista_documento_ids = $arquivosObj->recuperaIdListaDocumento($tipo_documento_id
                 </div>
                 <!-- /.card -->
             </div>
+
             <!-- /.col -->
             <div class="col-md-6">
                 <!-- Horizontal Form -->
                 <div class="card card-info">
                     <div class="card-header">
-                        <h3 class="card-title">Aquivos anexados</h3>
+                        <h3 class="card-title">Arquivos anexados</h3>
                     </div>
                     <!-- /.card-header -->
                     <!-- table start -->
@@ -122,13 +131,17 @@ $lista_documento_ids = $arquivosObj->recuperaIdListaDocumento($tipo_documento_id
                                     ?>
                                     <tr>
                                         <td><?= $arquivo->documento ?></td>
-                                        <td><a href="<?=SERVERURL."uploads/".$arquivo->arquivo?>" target="_blank"><?= mb_strimwidth($arquivo->arquivo, '15', '25', '...') ?></a></td>
+                                        <td><a href="<?= SERVERURL . "uploads/" . $arquivo->arquivo ?>"
+                                               target="_blank"><?= mb_strimwidth($arquivo->arquivo, '15', '25', '...') ?></a>
+                                        </td>
                                         <td><?= $arquivosObj->dataParaBR($arquivo->data) ?></td>
                                         <td>
-                                            <form class="formulario-ajax" action="<?=SERVERURL?>ajax/arquivosAjax.php" method="POST" data-form="delete">
+                                            <form class="formulario-ajax" action="<?= SERVERURL ?>ajax/arquivosAjax.php"
+                                                  method="POST" data-form="delete">
                                                 <input type="hidden" name="_method" value="removerArquivo">
-                                                <input type="hidden" name="pagina" value="<?=$_GET['views']?>">
-                                                <input type="hidden" name="arquivo_id" value="<?=$arquivosObj->encryption($arquivo->id)?>">
+                                                <input type="hidden" name="pagina" value="<?= $_GET['views'] ?>">
+                                                <input type="hidden" name="arquivo_id"
+                                                       value="<?= $arquivosObj->encryption($arquivo->id) ?>">
                                                 <button type="submit" class="btn btn-sm btn-danger">Apagar</button>
                                                 <div class="resposta-ajax"></div>
                                             </form>
@@ -139,7 +152,7 @@ $lista_documento_ids = $arquivosObj->recuperaIdListaDocumento($tipo_documento_id
                             } else {
                                 ?>
                                 <tr>
-                                    <td class="text-center" colspan="3">Nenhum arquivo enviado</td>
+                                    <td class="text-center" colspan="4">Nenhum arquivo enviado</td>
                                 </tr>
                                 <?php
                             }
@@ -158,7 +171,7 @@ $lista_documento_ids = $arquivosObj->recuperaIdListaDocumento($tipo_documento_id
 <!-- /.content -->
 
 <script>
-    function alerta(){
+    function alerta() {
         Swal.fire({
             title: 'FACC - Ficha de Atualização de Cadastro de Credores',
             html: 'A FACC é um documento necessário para recebimento do cachê. Após inserir seus dados pessoais e os dados bancários, clique no botão para gerar a FACC. <br><span style="color:red">Deve ser impressa, datada e assinada nos campos indicados no documento</span>.<br>Logo após, deve-se digitaliza-la e então anexa-la ao sistema através do campo abaixo.',
@@ -167,8 +180,8 @@ $lista_documento_ids = $arquivosObj->recuperaIdListaDocumento($tipo_documento_id
             allowEscapeKey: false,
             showCancelButton: false,
             confirmButtonText: 'Confirmar'
-        }).then(function() {
-            window.open('<?=$url?>', '_blank');
+        }).then(function () {
+            window.open('<?= SERVERURL ?>', '_blank');
         });
     }
 </script>
