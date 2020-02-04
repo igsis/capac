78d630f01563490b6bb4fdef919f18afe81daab3
Lucas Dauto <lucasdauto79@gmail.com>
2020-02-04 10:38:00 -0300

    fINALIZAR

---
+++ views/modulos/fomentos/finalizar.php
@@ -0,0 +1,62 @@
+<?php
+
+
+
+?>
+
+<!-- Content Header (Page header) -->
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0 text-dark">Finalizado Projeto</h1>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div>
+<!-- /.content-header -->
+<!-- Main content -->
+<div class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <!-- Horizontal Form -->
+                <div class="card card-info">
+                    <div class="card-header">
+                        <h3 class="card-title">Dados</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <ul id="lista-finalizar-fom">
+                        <li class="my-2"><span class="subtitulos mr-2">Código de cadastro:</span> 20200129.000001-te</li>
+                        <li class="my-2"><span class="subtitulos mr-2">Instituição responsável: </span> Ohhh Dono</li>
+                        <li class="my-2"><span class="subtitulos mr-2">Responsável pela inscrição: </span> Pessoa VIVA</li>
+                        <li class="my-2"><span class="subtitulos mr-2">Razão social: </span> SACOLINHAS LTDA.<span class="ml-5 subtitulos mr-2">CNPJ: </span> 00.000.000/000-00 </li>
+                        <li class="my-2"><span class="subtitulos mr-2">Representante Legal da empresa: </span> Qwerty Silva da Silva Sei <span class="ml-5 subtitulos mr-2">E-mail: </span> teste@test.com  <span class="ml-5 subtitulos mr-2">Telefone: </span> (11) 99999-9999</li>
+                        <li class="my-2"></li>
+                        <li class="my-2"><span class="subtitulos mr-2">CEP:</span> 12345-678 <span class="ml-5 subtitulos mr-2">Logradouro: </span> Rua AI <span class="ml-5 subtitulos mr-2">Número: </span>12000 <span class="ml-5 subtitulos mr-2">Bairro: </span>Bairros</li>
+                        <li class="my-2"><span class="subtitulos mr-2">Cidade: </span> Cidade  <span class="ml-5 subtitulos mr-2">Estado: </span> Estado</li>
+                        <li class="my-2"><span class="subtitulos mr-2">Site:</span> <a href="">https://www.google.com.br</a> </li>
+                        <li class="my-2"><span class="subtitulos mr-2">Valor do projeto:</span> <span id="dinheiro">999999.99</span></li>
+                        <li class="my-2"><span class="subtitulos mr-2"></span></li>
+                        <li class="my-2"><span class="subtitulos mr-2"></span></li>
+                        <li class="my-2"><span class="subtitulos mr-2"></span></li>
+                        <li class="my-2"><span class="subtitulos mr-2"></span></li>
+                        <li class="my-2"><span class="subtitulos mr-2"></span></li>
+                        <li class="my-2"><span class="subtitulos mr-2"></span></li>
+                        <li class="my-2"><span class="subtitulos mr-2"></span></li>
+                        <li class="my-2"><span class="subtitulos mr-2"></span></li>
+                        <li class="my-2"><span class="subtitulos mr-2"></span></li>
+                        <li class="my-2"><span class="subtitulos mr-2"></span></li>
+                        <li class="my-2"><span class="subtitulos mr-2"></span></li>
+                        <li class="my-2"><span class="subtitulos mr-2"></span></li>
+                        <li class="my-2"><span class="subtitulos mr-2"></span></li>
+                        <li class="my-2"><span class="subtitulos mr-2"></span></li>
+                    </ul>
+                </div>
+                <!-- /.card -->
+            </div>
+        </div>
+        <!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div>
+<!-- /.content -->
