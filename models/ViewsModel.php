1d8314e29c532dffbe65cf7b526f4ec98f3fbe70
Diego Vicente <diego.vdossantos@gmail.com>
2020-02-05 12:56:38 -0300

    Links retornando corretamente a home

--- models/ViewsModel.php
+++ models/ViewsModel.php
@@ -90,7 +90,8 @@ class ViewsModel
             if (is_file("./views/modulos/$modulo/include/menu.php")) {
                 $menu = "./views/modulos/$modulo/include/menu.php";
             } else {
-                $menu = "./views/template/menuExemplo.php";
+                /** @TODO: Encontrar forma para que a edição de conta retorne ao módulo correto */
+                $menu = "./views/modulos/fomentos/include/menu.php";
             }
         } else {
             $menu = "./views/template/menuExemplo.php";
