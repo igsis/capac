35db102375d67e62256c1d4b436eeaa5e1bc2016
Diego <diego.vdossantos@gmail.com>
2020-10-06 14:56:26 -0300

    comandos sql para o arquivo certo

--- #modelagem/fomentos_v2.sql
+++ #modelagem/fomentos_v2.sql
@@ -44,25 +44,3 @@ ALTER TABLE `fom_pf_dados`
 
 INSERT INTO `generos` (`genero`) VALUES ('Não Declarar');
 UPDATE `etnias` SET `descricao`='Não Declarar', `publicado`='1' WHERE `id`=1;
-
-alter table pf_detalhes
-    add trans tinyint default 0 null;
-
-alter table pf_detalhes
-    add pcd tinyint default 0 null;
-
-UPDATE `form_cargos` SET `cargo` = 'Articulador de Processos Artístico-Pedagógicos' WHERE `form_cargos`.`id` = 7;
-
-INSERT INTO `form_lista_documentos` (`id`, `documento`, `sigla`, `ordem`, `obrigatorio`, `publicado`) VALUES
-(24, 'Anexo II a IV (arquivo único)', 'f-anxII-IV', 0, 1, 1),
-(25, 'Anexo V (carta de intenção)', 'f-anexoV', 0, 1, 1),
-(26, 'Anexo VI (somente para opção de cotas étnico-raciais)', 'f-anexoVI', 0, 0, 1),
-(27, 'Anexo VII (somente para declarar opção de uso do nome social)', 'f-anexoVII', 0, 0, 1),
-(28, 'Laudo médico com CID (somente para pessoa com deficiência)', 'f-laudopcd', 0, 0, 1),
-(29, 'ANEXO VIII: Indicação de Membros da Sociedade Civil para a Comissão de Seleção', 'f-anxVIII', 0, 1, 1);
-
-UPDATE `etnias` SET `descricao` = 'Parda' WHERE `etnias`.`id` = 2;
-
-UPDATE `form_lista_documentos` SET `publicado` = '0' WHERE `form_lista_documentos`.`id` = 7;
-UPDATE `form_lista_documentos` SET `obrigatorio` = '0' WHERE `form_lista_documentos`.`id` = 6;
-UPDATE `form_lista_documentos` SET `obrigatorio` = '0' WHERE `form_lista_documentos`.`id` = 29;
