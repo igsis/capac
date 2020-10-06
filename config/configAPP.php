07e1e0644d4f1c0dfa5f23bad0e8645e75f9ba35
Diego <diego.vdossantos@gmail.com>
2020-10-06 16:13:57 -0300

    conexao2 ao bd siscontrat

--- config/configAPP.php
+++ config/configAPP.php
@@ -1,10 +1,16 @@
 <?php
     define('SERVER', "localhost");
-    define('DB', "capac_new");
-    define('USER', "root", true);
-    define('PASS', "", true);
 
-    define('SGDB', "mysql:host=".SERVER.";dbname=".DB);
+    define('DB1', "capac_new");
+    define('USER1', "root", true);
+    define('PASS1', "", true);
+
+    define('DB2', "siscontrat");
+    define('USER2', "root", true);
+    define('PASS2', "", true);
+
+    define('SGDB1', "mysql:host=".SERVER.";dbname=".DB1);
+    define('SGDB2', "mysql:host=".SERVER.";dbname=".DB2);
 
     define('METHOD', 'AES-256-CBC', true);
     define('SECRET_KEY', 'S3cr3t', true);
--- models/DbModel.php
+++ models/DbModel.php
@@ -9,9 +9,13 @@ class DbModel
 {
     public static $conn;
 
-    protected function connection() {
+    protected function connection($siscontrat = false) {
         if(!isset(self::$conn)) {
-            self::$conn = new PDO(SGDB, USER, PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+            if (!$siscontrat) {
+                self::$conn = new PDO(SGDB1, USER1, PASS1, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+            } else {
+                self::$conn = new PDO(SGDB2, USER2, PASS2, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+            }
             self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
         return self::$conn;
@@ -23,10 +27,12 @@ class DbModel
      * <p>Tabela do banco de dados</p>
      * @param array $data
      * <p>Dados a serem inseridos</p>
+     * @param bool $siscontrat
+     * <p><strong>FALSE</strong> por padrão. Quando <strong>TRUE</strong>, faz a consulta no banco de dados do sistema <i>SISCONTRAT</i></p>
      * @return bool|PDOStatement
      */
-    protected function insert($table, $data) {
-        $pdo = self::connection();
+    protected function insert($table, $data, $siscontrat = false) {
+        $pdo = self::connection($siscontrat);
         $fields = implode(", ", array_keys($data));
         $values = ":".implode(", :", array_keys($data));
         $sql = "INSERT INTO $table ($fields) VALUES ($values)";
@@ -40,8 +46,20 @@ class DbModel
     }
 
     // Método para update
-    protected function update($table, $data, $id){
-        $pdo = self::connection();
+    /**
+     * <p>Atualiza os dados do registro especificado</p>
+     * @param string $table
+     * <p>Tabela do banco de dados</p>
+     * @param array $data
+     * <p>Dados a serem inseridos</p>
+     * @param int $id
+     * <p>ID do registro a ser atualizado</p>
+     * @param bool $siscontrat
+     * <p><strong>FALSE</strong> por padrão. Quando <strong>TRUE</strong>, faz a consulta no banco de dados do sistema <i>SISCONTRAT</i></p>
+     * @return bool|PDOStatement
+     */
+    protected function update($table, $data, $id, $siscontrat = false){
+        $pdo = self::connection($siscontrat);
         $new_values = "";
         foreach($data as $key => $value) {
             $new_values .= "$key=:$key, ";
@@ -59,8 +77,23 @@ class DbModel
     }
 
     // Método para update especial
-    protected function updateEspecial($table, $data, $campo, $campo_id){
-        $pdo = self::connection();
+
+    /**
+     * <p>Atualiza os dados de um registro onde o campo buscado não é o <i>id</i></p>
+     * @param string $table
+     * <p>Tabela do banco de dados</p>
+     * @param array $data
+     * <p>Dados a serem inseridos</p>
+     * @param array $campo
+     * <p>Campo pelo qual deve ser pesquisado o registro</p>
+     * @param array|string $campo_id
+     * <p><i>Valor</i> do registro a ser atualizado</p>
+     * @param bool $siscontrat
+     * <p><strong>FALSE</strong> por padrão. Quando <strong>TRUE</strong>, faz a consulta no banco de dados do sistema <i>SISCONTRAT</i></p>
+     * @return bool|PDOStatement
+     */
+    protected function updateEspecial($table, $data, $campo, $campo_id, $siscontrat = false){
+        $pdo = self::connection($siscontrat);
         $new_values = "";
         foreach($data as $key => $value) {
             $new_values .= "$key=:$key, ";
@@ -78,8 +111,19 @@ class DbModel
     }
 
     // Método para apagar (despublicar)
-    protected function apaga($table, $id){
-        $pdo = self::connection();
+
+    /**
+     * <p>Método para despublicar o registro especificado</p>
+     * @param $table
+     * <p>Tabela do banco de dados</p>
+     * @param $id
+     * <p>ID do registro que deve ser despublicado</p>
+     * @param bool $siscontrat
+     * <p><strong>FALSE</strong> por padrão. Quando <strong>TRUE</strong>, faz a consulta no banco de dados do sistema <i>SISCONTRAT</i></p>
+     * @return bool|PDOStatement
+     */
+    protected function apaga($table, $id, $siscontrat = false){
+        $pdo = self::connection($siscontrat);
         $sql = "UPDATE $table SET publicado = 0 WHERE id = :id";
         $statement = $pdo->prepare($sql);
         $statement->bindValue(":id", $id);
@@ -88,8 +132,20 @@ class DbModel
         return $statement;
     }
 
-    protected function deleteEspecial($table, $campo, $campo_id){
-        $pdo = self::connection();
+    /**
+     * <p>Este método <strong>DELETA</strong> o registro selecionado do sistema. <strong>ATENÇÃO! Este processo não tem reversão</strong></p>
+     * @param $table
+     * <p>Tabela do banco de dados</p>
+     * @param $campo
+     * <p>Campo pelo qual deve ser pesquisado o registro</p>
+     * @param $campo_id
+     * <p><i>Valor</i> do registro que será <strong>EXCLUÍDO</strong></p>
+     * @param bool $siscontrat
+     * <p><strong>FALSE</strong> por padrão. Quando <strong>TRUE</strong>, faz a consulta no banco de dados do sistema <i>SISCONTRAT</i></p>
+     * @return bool|PDOStatement
+     */
+    protected function deleteEspecial($table, $campo, $campo_id, $siscontrat = false){
+        $pdo = self::connection($siscontrat);
         $sql = "DELETE FROM $table WHERE $campo = :$campo";
         $statement = $pdo->prepare($sql);
         $statement->bindValue(":$campo", $campo_id, PDO::PARAM_STR);
@@ -98,8 +154,8 @@ class DbModel
         return $statement;
     }
 
-    public function consultaSimples($consulta) {
-        $pdo = self::connection();
+    public function consultaSimples($consulta, $siscontrat = false) {
+        $pdo = self::connection($siscontrat);
         $statement = $pdo->prepare($consulta);
         $statement->execute();
 
@@ -112,8 +168,8 @@ class DbModel
      * @param $id
      * @return bool|PDOStatement
      */
-    protected function getInfo($table, $id){
-        $pdo = self::connection();
+    protected function getInfo($table, $id, $siscontrat = false){
+        $pdo = self::connection($siscontrat);
         $sql = "SELECT * FROM $table WHERE id = :id";
         $statement = $pdo->prepare($sql);
         $statement->bindValue(":id", $id);
@@ -128,8 +184,8 @@ class DbModel
      * @param $id
      * @return bool|PDOStatement
      */
-    protected function getInfoEspecial($table, $campo, $campo_id){
-        $pdo = self::connection();
+    protected function getInfoEspecial($table, $campo, $campo_id, $siscontrat = false){
+        $pdo = self::connection($siscontrat);
         $sql = "SELECT * FROM $table WHERE $campo = :campo_id";
         $statement = $pdo->prepare($sql);
         $statement->bindValue(":campo_id", $campo_id);
@@ -139,14 +195,14 @@ class DbModel
     }
 
     // Lista publicados
-    protected function listaPublicado($table,$id = null) {
+    protected function listaPublicado($table,$id = null, $siscontrat = false) {
         if(!empty($id)){
             $filtro_id = "AND id = :id";
         }
         else{
             $filtro_id = "";
         }
-        $pdo = self::connection();
+        $pdo = self::connection($siscontrat);
         $sql = "SELECT * FROM $table WHERE publicado = 1 $filtro_id ORDER BY 2";
         $statement = $pdo->query($sql);
         $statement->execute();
