96977a7b94ed8654e8482b97940e4f7221e69bb4
Diego <diego.vdossantos@gmail.com>
2020-10-23 16:54:27 -0300

    remoção do bloqueio da seleção de articuladores em formação

--- api/api_formacao_cargos.php
+++ api/api_formacao_cargos.php
@@ -12,8 +12,6 @@ header('Content-Type: application/json');
 
 
 if(isset($_GET['busca'])){
-    $articuladores = [3, 6, 7];
-
     if ($_GET['busca'] == 1) {
         $programa_id = $_GET['programa_id'];
         $sql = "SELECT cp.formacao_cargo_id AS 'id', fc.cargo AS 'cargo' FROM cargo_programas AS cp
@@ -25,8 +23,7 @@ if(isset($_GET['busca'])){
     }
     elseif ($_GET['busca'] == 2) {
         $programa_id = $_GET['programa_id'];
-        $cargo1_id = $_GET['cargo1_id'];
-        $cargo1 = in_array($cargo1_id, $articuladores) ? implode(", ", $articuladores) : $cargo1_id;
+        $cargo1 = $_GET['cargo1_id'];
 
         $sql = "SELECT DISTINCT cp.formacao_cargo_id AS 'id', fc.cargo AS 'cargo' FROM cargo_programas AS cp
                 INNER JOIN siscontrat.formacao_cargos AS fc ON cp.formacao_cargo_id = fc.id
@@ -40,13 +37,7 @@ if(isset($_GET['busca'])){
         $programa_id = $_GET['programa_id'];
         $cargo1 = $_GET['cargo1_id'];
         $cargo2 = $_GET['cargo2_id'];
-
-        if (in_array($cargo1, $articuladores)) {
-            $cargo1 = implode(", ", $articuladores);
-        } elseif (in_array($cargo2, $articuladores)) {
-            $cargo2 = implode(", ", $articuladores);
-        }
-
+        
         $sql = "SELECT DISTINCT cp.formacao_cargo_id AS 'id', fc.cargo AS 'cargo' FROM cargo_programas AS cp
                 INNER JOIN siscontrat.formacao_cargos AS fc ON cp.formacao_cargo_id = fc.id
                 WHERE cp.programa_id = '$programa_id' AND cp.formacao_cargo_id NOT IN ($cargo1, $cargo2, 4, 5) ORDER BY fc.cargo";
