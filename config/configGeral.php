fd691fe70a29db2d75744cfc5d5a9a7c84376860
Lorelei <loreleigab@hotmail.com>
2020-11-09 09:12:58 -0300

    Alterando o timezone para Fortaleza para corrigir horário de verão

--- config/configGeral.php
+++ config/configGeral.php
@@ -1,5 +1,5 @@
 <?php
 define('SERVERURL', "http://{$_SERVER['HTTP_HOST']}/capac/");
 define('NOMESIS', "CAPAC");
-date_default_timezone_set('America/Sao_Paulo');
+date_default_timezone_set('America/Fortaleza');
 ini_set('session.gc_maxlifetime', 60*60); // 60 minutos
